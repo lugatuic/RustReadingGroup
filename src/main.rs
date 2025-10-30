979255cd5fac35064fdc15d32b06d6134d3f95a2
Samuel Skean <sskean2@uic.edu>
2025-10-30 17:14:27 -0500

    After second meeting.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub mod first;
 fn main() {
     let x = Box::new(54);
     baz(x);
+    // We discussed this error in detail! It's related to ownership ([covered in "the book", here](https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html)).
     println!("{x}");
 }
 
