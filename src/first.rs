826a6d6bf924cbf11b259700fdb0b3ea25e619f5
Samuel Skean <sskean2@uic.edu>
2025-11-13 16:34:07 -0600

    After 4th meeting.

--- src/first.rs
+++ src/first.rs
@@ -26,5 +26,56 @@ impl List {
         self.head = new_link;
     }
 
-    pub fn pop(&mut self) -> Option<i32> {}
+    pub fn pop(&mut self) -> Option<i32> {
+        match std::mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(first) => {
+                self.head = first.next;
+                Some(first.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = std::mem::replace(&mut self.head, Link::Empty);
+        // We'll look at this error next time!
+        while let Link::More(next) = cur_link {
+            cur_link = std::mem::replace(&mut next.next, Link::Empty);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    pub fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
 }
